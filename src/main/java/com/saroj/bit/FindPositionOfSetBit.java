d7f2620114965b76624619b3518448826de069fe
Saroj Rout <saroj@Saroj.local>
2016-05-04 13:26:04 -0700

    basic bit problem

--- src/main/java/com/saroj/bit/FindPositionOfSetBit.java
+++ src/main/java/com/saroj/bit/FindPositionOfSetBit.java
@@ -2,13 +2,13 @@ package com.saroj.bit;
 
 public class FindPositionOfSetBit {
 	public static void main(String[] args) {
-		int result = FindPositionOfSetBit.findPos(10);
+		int result = FindPositionOfSetBit.findPos(6);
 		System.out.println(result);
 	}
 
 	private static int findPos(int data){
 		double result = Math.log(data) / Math.log(2);
-		if(result%2==0){
+		if(result%1==0){
 			return (int)result+1;
 		}
 		else{
